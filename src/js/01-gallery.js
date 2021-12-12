c737d75f21f7dd0d6b0eae2a0780c53d90d724c1
CoolhanD <abrawa@gmail.com>
2021-12-12 16:38:52 +0200

    image gallery compiled

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const refs = {
+  imgGalleryContainer: document.querySelector(".gallery"),
+};
+// console.log(galleryItems);
+const imagesItems = galleryItems.map((option) => {
+  const imageEl = document.createElement("img");
+  imageEl.src = option.preview;
+  imageEl.alt = option.description;
+  imageEl.classList.add("gallery__image");
+  return imageEl;
+});
 
-console.log(galleryItems);
+refs.imgGalleryContainer.append(...imagesItems);
