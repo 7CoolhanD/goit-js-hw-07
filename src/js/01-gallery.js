542e42bbfde3302ae724c45656535902a0439231
CoolhanD <abrawa@gmail.com>
2021-12-14 18:18:32 +0200

    created modal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
-const refs = {
-  imgGalleryContainer: document.querySelector(".gallery"),
-};
-// console.log(galleryItems);
-const imagesItems = galleryItems.map((option) => {
-  const imageEl = document.createElement("img");
-  imageEl.src = option.preview;
-  imageEl.alt = option.description;
-  imageEl.classList.add("gallery__image");
-  return imageEl;
-});
 
-refs.imgGalleryContainer.append(...imagesItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryImgItems = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+refs.gallery.insertAdjacentHTML("afterbegin", galleryImgItems);
+
+refs.gallery.addEventListener("click", openImgInModal);
+
+function openImgInModal(e) {
+  e.preventDefault();
+  const isImage = e.target.classList.contains("gallery__image");
+  if (!isImage) {
+    instance.close();
+  }
+  const instance = basicLightbox.create(`
+  <div class="modal">
+  <img src=${e.target.dataset.source}>
+  </div>
+  `);
+  instance.show();
+}
